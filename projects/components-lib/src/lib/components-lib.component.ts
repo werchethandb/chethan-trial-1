d0d03d736e5870b015f0237d2c4a402a95d36c40
werchethandb <chethan.balakrishna@werner.com>
2022-08-10 10:11:24 -0500

    first commit

---
+++ projects/components-lib/src/lib/components-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cmpts-ComponentsLib',
+  template: `
+    <p>
+      components-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentsLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
